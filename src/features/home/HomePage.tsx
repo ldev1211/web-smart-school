f49ef0fc5ce1690b0e61b4d27220a58e153844a2
Luong Dien <ldev@Luongs-MacBook-Pro.local>
2024-03-27 23:06:44 +0700

    dqwdqw

--- src/features/home/HomePage.tsx
+++ src/features/home/HomePage.tsx
@@ -3,21 +3,7 @@ import axios from "axios";
 
 function HomePage() {
   const [isLoading, setLoading] = useState(true);
-  useEffect(() => {
-    axios({
-      url: "http://localhost:3000/teacher/login",
-      method: "POST",
-      data: { username: "pthy.edu", password: "123" },
-    })
-      .then((response) => {
-        console.log(response);
-        setLoading(false);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  }, []);
-  return <h1>{isLoading ? "Loading..." : "Loaded"}</h1>;
+  return <h1>Hi</h1>;
 }
 
 export default HomePage;
